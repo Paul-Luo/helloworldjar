51d6ff0c7fdc695ef8a40fc05f8f530c6d0bebc7
lch <234750677@qq.com>
2014-12-11 11:33:09 +0800

    打印环境变量系统变量和参数

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,12 +2,12 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.DateFormat;
-import java.text.FieldPosition;
-import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * author : lch
@@ -15,13 +15,17 @@ import java.util.Date;
  * depiction :
  */
 public class HelloWorld {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        printToFile(args);
+    }
+
+
+    public static void printToFile(String[] args) throws Exception {
         File outDir = new File("/home/vagrant/output");
         if (!outDir.exists()) {
             outDir.mkdirs();
         }
         DateFormat df = new SimpleDateFormat("HH-mm-ss");
-
 
         File outputFile = new File(outDir, "output_" + df.format(new Date()) + ".log");
         System.out.println(outputFile.getCanonicalPath());
@@ -30,13 +34,34 @@ public class HelloWorld {
         }
 
         if (args == null || args.length == 0) {
-            FileUtils.writeStringToFile(outputFile, "args is null");
+            FileUtils.writeStringToFile(outputFile, "args is null" + IOUtils.LINE_SEPARATOR_UNIX);
         } else {
-            for (String s : args) {
-                s = s + IOUtils.LINE_SEPARATOR_UNIX;
-                FileUtils.writeStringToFile(outputFile, s, true);
+            for (int i = 0; i < args.length; i++) {
+                String s = args[i];
+                FileUtils.writeStringToFile(outputFile, "arg[" + i + "] is " + s + IOUtils.LINE_SEPARATOR_UNIX, true);
             }
-            FileUtils.writeStringToFile(outputFile, "args length is " + args.length, true);
+            FileUtils.writeStringToFile(outputFile, "args length is " + args.length + IOUtils.LINE_SEPARATOR_UNIX, true);
         }
+
+        // print sysproperties
+        Map m = System.getenv();
+        FileUtils.writeStringToFile(outputFile, "------------------ env -- start -----------------------" + IOUtils.LINE_SEPARATOR_UNIX, true);
+        for (Iterator it = m.keySet().iterator(); it.hasNext(); ) {
+            String key = (String) it.next();
+            String value = (String) m.get(key);
+            System.out.println(key + ":" + value);
+            FileUtils.writeStringToFile(outputFile, key + ":" + value + IOUtils.LINE_SEPARATOR_UNIX, true);
+        }
+        FileUtils.writeStringToFile(outputFile, "------------------ env -- end -----------------------" + IOUtils.LINE_SEPARATOR_UNIX, true);
+        FileUtils.writeStringToFile(outputFile, IOUtils.LINE_SEPARATOR_UNIX, true);
+        FileUtils.writeStringToFile(outputFile, "---------------- jvm -- start ---------------------" + IOUtils.LINE_SEPARATOR_UNIX, true);
+        Properties p = System.getProperties();
+        for (Iterator it = p.keySet().iterator(); it.hasNext(); ) {
+            String key = (String) it.next();
+            String value = (String) p.get(key);
+            System.out.println(key + ":" + value);
+            FileUtils.writeStringToFile(outputFile, key + " ---> " + value + IOUtils.LINE_SEPARATOR_UNIX, true);
+        }
+        FileUtils.writeStringToFile(outputFile, "---------------- jvm -- end ---------------------" + IOUtils.LINE_SEPARATOR_UNIX, true);
     }
 }
